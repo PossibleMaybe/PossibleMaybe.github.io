ea5c882eff3a0bd6705c624302273059b2eeaf81
wuyongchun <yongchun.wu@aihuishou.com>
2018-04-04 15:23:12 +0800

    type:feature profile: module: description:初始化项目

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-var cacheName = 'hello,world';
-self.addEventListener('install',event => {
-    event.waitUntil(caches.open(cacheName).then( cache => cache.addAll(['./js/script.js','./img/94cad1c8a786c9179e80a80cc23d70cf3bc75700.jpg'])));
-})
